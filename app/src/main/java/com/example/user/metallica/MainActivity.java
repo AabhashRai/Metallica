1080fa516407b9823537578d649bd5216a35ad03
AabhashRai <raiaabhash4@gmail.com>
2017-06-21 08:38:01 -0700

    second commit

---
+++ app/src/main/java/com/example/user/metallica/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.user.metallica;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer Song;
+    int pause;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void play(View view) {
+        if (Song == null) {
+            Song = MediaPlayer.create(this, R.raw.hardwired);
+            Song.start();
+        } else if (!Song.isPlaying()) {
+            Song.seekTo(pause);
+            Song.start();
+            Toast.makeText(MainActivity.this, "Song play", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void pause(View view) {
+        if (Song != null) {
+            Song.pause();
+            pause = Song.getCurrentPosition();
+        }
+        Toast.makeText(MainActivity.this, "Song pause", Toast.LENGTH_SHORT).show();
+    }
+
+    public void stop(View view) {
+        Song.stop();
+        Song = null;
+        Toast.makeText(MainActivity.this, "Song stop", Toast.LENGTH_SHORT).show();
+    }
+}
+
